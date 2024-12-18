61e1fccd19e2af815d3b26a6fe3821b608b21cab
Marek Szczegodziński <mszczegodzinski@o2.pl>
2024-12-18 01:14:57 +0100

    feat(DB config): add posstgresql db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,19 @@ import { Module } from '@nestjs/common';
 import { RecipeModule } from './recipe/recipe.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { databaseConfig } from './config/database.config';
+import { envValidationSchema } from './config/envValidation.config';
 
 @Module({
   imports: [
     RecipeModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: './database/my-db.sqlite3',
-      autoLoadEntities: true,
-      synchronize: true, // not for production
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync(databaseConfig),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
+      validationSchema: envValidationSchema,
     }),
     UserModule,
   ],
